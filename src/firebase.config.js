2838ece0c1741338f27dc1af7524c9a9d68df02c
dzikrialifa <dzikrialif99@gmail.com>
2020-04-16 14:14:20 +0700

    Firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBYfC384Qe88aTgiVZL6TlgNd_7VMgF9YU",
+    authDomain: "reactfirebase-7f158.firebaseapp.com",
+    databaseURL: "https://reactfirebase-7f158.firebaseio.com",
+    projectId: "reactfirebase-7f158",
+    storageBucket: "reactfirebase-7f158.appspot.com",
+    messagingSenderId: "366105890592",
+    appId: "1:366105890592:web:20ff223a4d17713e845145",
+    measurementId: "G-2YDQEMM226"
+}
